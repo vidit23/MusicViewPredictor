a69c10ac5083989edce08644a92858bcd574de42
Nandhitha Raghuram <nandhitharaghuram@Nandhithas-MacBook-Pro.local>
2020-03-21 20:36:02 -0400

    get the youtube id for songs

--- requester.py
+++ requester.py
@@ -5,6 +5,9 @@ from spotipy.oauth2 import SpotifyClientCredentials
 
 from config import *
 
+import os
+import googleapiclient.discovery
+
 spotifyCredentialsManager = SpotifyClientCredentials(client_id = SPOTIFY_CLIENT_ID, client_secret = SPOTIFY_CLIENT_SECRET)
 spotifyClient = spotipy.Spotify(client_credentials_manager=spotifyCredentialsManager)
 
@@ -29,3 +32,41 @@ def getSpotifyTrackDetails(trackIds):
     result = pd.merge(infoDf, featuresDf, how='inner', on='id', sort=True, copy=True, indicator=False, validate=None)
     result.rename({'id': '_id'}, axis=1, inplace=True)
     return result
+
+def getSongList(trackIds):
+    spotifyTracksDf=getSpotifyTrackDetails(trackIds)
+    spotifyIdArtists = spotifyTracksDf[['artists','_id']]
+    spotifyArtists = spotifyTracksDf['artists'].values.tolist()
+    flat_list = []
+    for sublist in spotifyArtists:
+        for item in sublist:
+            flat_list.append(item)
+    spotifyArtists=pd.DataFrame(flat_list)
+    spotifyArtists.columns=['artist_id','artist_name']
+    spotifyMergedDf=pd.concat([spotifyTracksDf,spotifyArtists], axis=1)
+    song_list = []
+    for index,row in spotifyMergedDf.iterrows():
+        song = row['artist_name'] +" "+row['name']
+        song_list.append(song)
+    return song_list
+
+def getYouTubeIds(trackIds):
+    song_list = getSongList(trackIds)
+    youtubeSongList=[]
+    youtubeIdList=[]
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+    api_service_name = "youtube"
+    api_version = "v3"
+    DEVELOPER_KEY = YOUTUBE_API_KEY
+    youtube = googleapiclient.discovery.build(
+        api_service_name, api_version, developerKey = DEVELOPER_KEY)
+    for query in song_list:
+        request = youtube.search().list(
+            part="snippet",
+            maxResults=25,
+            q=query
+        )
+        youtubeSongList.append(response)
+    for songs in youtubeSongList:
+        youtubeIdList.append(songs['items'][0])
+    return youtubeIdList

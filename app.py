fbc4b3a0ec0ef332e48158d04fe33343819055b5
vidit23 <viditvineet@hotmail.com>
2020-03-31 00:18:50 -0400

    Added the fetching of youtube ids

--- app.py
+++ app.py
@@ -6,15 +6,18 @@ from requester import *
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/spotifyTracks")
 def main():
     spotifyTracksDf = getSpotifyTrackDetails(['3n3Ppam7vgaVa1iaRUc9Lp','3twNvmDtFQtAd5gMKedhLD'])
     result = insertManyFromDataframe('Videos', spotifyTracksDf)
     return str(result)
 
-@app.route('/how are you')
-def hello():
-    return 'I am good, how about you?'
+@app.route('/youtubeIds')
+def getCorrespondingYoutubeIds():
+    correspondingIds = getYouTubeIds()
+    for dicts in correspondingIds:
+        updateOneDocument('Videos', dicts['_id'], dicts)
+    return str(correspondingIds)
 
 
 if __name__ == "__main__":
--- requester.py
+++ requester.py
@@ -2,14 +2,18 @@ import pandas as pd
 import requests
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+import googleapiclient.discovery
+
 
 from config import *
+import models
 
 import os
-import googleapiclient.discovery
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
 spotifyCredentialsManager = SpotifyClientCredentials(client_id = SPOTIFY_CLIENT_ID, client_secret = SPOTIFY_CLIENT_SECRET)
 spotifyClient = spotipy.Spotify(client_credentials_manager=spotifyCredentialsManager)
+youtubeClient = googleapiclient.discovery.build('youtube', 'v3', developerKey = YOUTUBE_API_KEY)
 
 
 def getSpotifyTrackDetails(trackIds):
@@ -33,73 +37,44 @@ def getSpotifyTrackDetails(trackIds):
     result.rename({'id': '_id'}, axis=1, inplace=True)
     return result
 
-def getCursorOfSize(collectionName, batchSize):
-    count = 1
-    finalResult = []
-    tracks = []
-    for doc in connectedDB[collectionName].find(batch_size=batchSize):
-        if count % batchSize == 0:
-            finalResult.append(tracks)
-            tracks = []
-        count+=1
-        tracks.append(doc)
-    finalResult.append(tracks)
-    return finalResult
 
-def getSongList():
-    spotifyTracksDf = getCursorOfSize('Videos', 50)
+def getYoutubeSearchQueries():
+    spotifyTracksDf = models.getCursorOfSize('Videos', {'youtubeId': {'$exists': False}}, 50)
     spotifySongs = []
-    count = 1
     for row in spotifyTracksDf:
         for song in row:
-            s_name = song['name']
+            query = song['name']
             for artist in song['artists']:
                 a_name = artist['name']
-                s = s_name + ' '+ a_name
-                spotifySongs.append({'_id': song['_id'], 'name': s})
+                query = query + ' '+ a_name
+            spotifySongs.append({'_id': song['_id'], 'name': query})
     return spotifySongs
 
 #returns videoIds, channelId, title description and other information related to the video
 def getYouTubeIds():
-    song_list = getSongList()
-    song_list = song_list[:5]
+    song_list = getYoutubeSearchQueries()
+    song_list = song_list[:2]
     youtubeSongList=[]
-    youtubeIdList=[]
-    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
-    api_service_name = "youtube"
-    api_version = "v3"
-    youtube = googleapiclient.discovery.build(
-        api_service_name, api_version, developerKey = YOUTUBE_API_KEY)
     for query in song_list:
-        request = youtube.search().list(
-            part="snippet",
-            maxResults=25,
-            q=query['name']
-        )
+        request = youtubeClient.search().list(part="id", maxResults=1, type='video', q=query['name'])
         response = request.execute()
         youtubeSongList.append({'_id': query['_id'], 'youtubeId': response['items'][0]['id']['videoId']})
     return youtubeSongList
 
 
-def getVideoStatistics():
-    videoData =[]
-    youTubeVideoList=[]
-    videoIdList=getVideoIds()
-    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
-    api_service_name = "youtube"
-    api_version = "v3"
-    youtube = googleapiclient.discovery.build(
-        api_service_name, api_version, developerKey = YOUTUBE_API_KEY)
-    for ids in videoIdList:
-        request = youtube.videos().list(
-        part="snippet,contentDetails,statistics", id=ids)
-        response = request.execute()
-        youTubeVideoList.append(response)
-    for items in youTubeVideoList:
-        for data in items['items']:
-            details = {
-                'id':data['id'],
-                'statistics':data['statistics']}
-            videoData.append(details)
-    return videoData
+# def getVideoStatistics():
+#     videoData =[]
+#     youTubeVideoList=[]
+#     videoIdList=getVideoIds()
+#     for ids in videoIdList:
+#         request = youtubeClient.videos().list(part="snippet,contentDetails,statistics", id=ids)
+#         response = request.execute()
+#         youTubeVideoList.append(response)
+#     for items in youTubeVideoList:
+#         for data in items['items']:
+#             details = {
+#                 'id':data['id'],
+#                 'statistics':data['statistics']}
+#             videoData.append(details)
+#     return videoData
     

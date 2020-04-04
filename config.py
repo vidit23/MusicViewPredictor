dc2e09e619bafae53a48afb5ad32bc20ee0618ec
vidit23 <viditvineet@hotmail.com>
2020-04-04 01:58:44 -0400

    Added config

--- config.py
+++ config.py
@@ -1,5 +1,13 @@
-YOUTUBE_API_KEY = ""
-SPOTIFY_CLIENT_ID = ""
-SPOTIFY_CLIENT_SECRET = ""
-MONGO_ATLAS_USER = ""
-MONGO_ATLAS_PASSWORD = ""
+# YOUTUBE_API_KEY = "AIzaSyBJv8R8JsHsczjILQC9opv6R9UDoa0qhc8"
+YOUTUBE_API_KEY = [ "AIzaSyA84XA2PZtFhJSIAChcIUJ2IZTwxr3sJVk", "AIzaSyCXe8E-YUGdX98wwelPrSg96pk11aQIH-Q",
+                    "AIzaSyCPjM_EjumIu0f_ntIout6P_RwRBXZLcKM", "AIzaSyBjXVusxr4ZT12Iq4RAj4XvpFaHCsgjfGA",
+                    "AIzaSyCV86mhP-w-eHiGxAW94mF__K6tPsgDSqI", "AIzaSyBJv8R8JsHsczjILQC9opv6R9UDoa0qhc8",
+                    "AIzaSyAzkGNpcJpiaZBPW_x5bchMVthXdUj1f7c", "AIzaSyCmaXk7TyOiWCxpFfD_Q3bgKpTtCGSI9og",
+                    "AIzaSyBJRLOs3Jk2mxdiuDxo4Kg0Rs1aSTT7mKM", "AIzaSyC5IvLOmAvullVSu2ocg8rCI6x9txG6ugw", 
+                    "AIzaSyDMTNdKFkiljI2vvsbCQKrTH1kriSlEoto", "AIzaSyCk8W6Sjbdl8ep3JY39iLf4M0ayOfuCWxk",
+                    "AIzaSyCY-Xje40TUIyVH7Q4_IQcx2Cb3OnGyS50"]
+YOUTUBE_KEY_NUMBER = 0
+SPOTIFY_CLIENT_ID = "b3d03dff0ec94c07a0fb7b1056a94a0a"
+SPOTIFY_CLIENT_SECRET = "6c61cbeaa7c2483dbf6e78f7313b4a0c"
+MONGO_ATLAS_USER = "vidit23"
+MONGO_ATLAS_PASSWORD = "dsba123"
